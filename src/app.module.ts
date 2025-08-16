aeb2ef6f8d61623740287e42c4ef1f3af1f2e0eb
kbo025 <kbo025@gmail.com>
2025-08-16 11:43:45 -0300

    feat: libs and configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { config, configSchema, enviroments } from './config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: enviroments[process.env.NODE_ENV ?? 'development'] || '.env',
+      load: [config],
+      isGlobal: true,
+      validationSchema: configSchema,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config.ts
@@ -0,0 +1,42 @@
+import { registerAs } from '@nestjs/config';
+import * as Joi from 'joi';
+
+export const enviroments = {
+  dev: '.env',
+  test: '.test.env',
+  prod: '.prod.env',
+};
+
+export const config = registerAs('config', () => {
+  return {
+    pg: {
+      name: process.env.POSTGRES_NAME,
+      user: process.env.POSTGRES_USER,
+      pass: process.env.POSTGRES_PASS,
+      port: process.env.POSTGRES_PORT,
+      host: process.env.POSTGRES_HOST,
+    },
+    jwt: {
+      secret: process.env.JWT_SECRET,
+      expireIn: process.env.JWT_EXPIRE_IN,
+    },
+    apiKey: process.env.API_KEY,
+    superadminRole: process.env.SUPERADMIN_ROLE,
+    superadminEmail: process.env.SUPERADMIN_EMAIL,
+    rbacInsert: process.env.RBAC_INSERT,
+  };
+});
+
+export const configSchema = Joi.object({
+  API_KEY: Joi.number().required(),
+  POSTGRES_NAME: Joi.string().required(),
+  POSTGRES_USER: Joi.string().required(),
+  POSTGRES_PASS: Joi.string().required(),
+  POSTGRES_PORT: Joi.number().required(),
+  POSTGRES_HOST: Joi.string().hostname().required(),
+  JWT_SECRET: Joi.string().required(),
+  JWT_EXPIRE_IN: Joi.string().required(),
+  SUPERADMIN_ROLE: Joi.string().required(),
+  SUPERADMIN_EMAIL: Joi.string().required().email(),
+  RBAC_INSERT: Joi.boolean(),
+});
